a46aaf2a028d39dc8207c8ad7e74d7264055c423
Denys Sobchyshak <sobchysh@informatik.tu-muenchen.de>
2012-10-30 13:47:37 +0100

    created skeleton for python search script

---
+++ fundamental-algorithms/Search.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+__author__ = "Denys Sobchyshak"
+__email__ = "denys.sobchyshak@gmail.com"
+__version__ = "1.0.0"
+
+
+class Search:
+
+    def __init__(self):
+        #some instantiation code here
+        pass
+
+    def insertion_sort(self, list_to_sort):
+        #do something here
+        pass
+
+
+def main():
+    #main code here
+    print 'This is only a test'
+    pass
+
+if __name__ == '__main__' : main()
