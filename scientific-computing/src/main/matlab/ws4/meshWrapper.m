c051cdf077614ffaf478d2ffb11bf5795e769920
Denys S <denys.sobchyshak@gmail.com>
2012-12-17 10:27:29 +0100

    improved comments

--- scientific-computing/src/main/matlab/ws4/meshWrapper.m
+++ scientific-computing/src/main/matlab/ws4/meshWrapper.m
@@ -1,6 +1,6 @@
 function [ W ] = meshWrapper( X,Nx,Ny )
 %MESHWRAPPER Maps a vector to a mesh and puts zeroes around it's borders.
-%   Thus, with for a 4x1 vector [A11 A21 A12 A22] one will get a 4x4 matrix
+%   Thus, for a 4x1 vector [A11 A21 A12 A22] one will get a 4x4 matrix
 %   of the form [0 0 0 0;0 A11 A12 0;0 A21 A22 0;0 0 0 0];
     W = zeros(Nx+2,Ny+2);
     
