5b8ce80dfeebe39b78c6aa33b59359b5be3085b7
Denys S <denys.sobchyshak@gmail.com>
2013-01-22 22:11:31 +0100

    misc:formatting in unittest

--- fundamental-algorithms/src/test/cpp/sorter_unittest.cc
+++ fundamental-algorithms/src/test/cpp/sorter_unittest.cc
@@ -17,93 +17,93 @@
 #include "sorter.h"
 #include "gtest/gtest.h"
 
-int array_size = 100;
-int max = 300;
+int _array_size = 100;
+int _max = 300;
 
 void FillNonUniqueRandomNumbers(int array[], int max_value){
-    for (int i=0; i<array_size; i++)
+    for (int i=0; i<_array_size; i++)
         array[i] = std::rand()%max_value;
 }
 
 //Tests BubbleSort()
 TEST(BubbleSortTest, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    BubbleSort(a, array_size);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    BubbleSort(a, _array_size);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests InsertionSort()
 TEST(InsertionSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    InsertionSort(a, array_size);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    InsertionSort(a, _array_size);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests ShellSort()
 TEST(ShellSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    ShellSort(a, array_size);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    ShellSort(a, _array_size);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests SelectionSort()
 TEST(SelectionSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    SelectionSort(a, array_size);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    SelectionSort(a, _array_size);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests MergeSort()
 TEST(MergeSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    MergeSort(a, 0, array_size-1);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    MergeSort(a, 0, _array_size-1);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests QuickSort()
 TEST(QuickSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    QuickSort(a, 0, array_size-1);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    QuickSort(a, 0, _array_size-1);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests QuickSort3()
 TEST(QuickSort3, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    QuickSort3(a, 0, array_size-1);
-    for (int i=1; i<array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
+    QuickSort3(a, 0, _array_size-1);
+    for (int i=1; i<_array_size; i++) ASSERT_TRUE(a[i-1] <= a[i]);
 }
 
 //Tests HeapSort()
 TEST(HeapSort, NonUniqueRandom){
-    int a[array_size];
+    int a[_array_size];
     FillNonUniqueRandomNumbers(a, INT_MAX);
-    HeapSort(a, array_size);
-    for (int i=1; i<array_size; i++)
+    HeapSort(a, _array_size);
+    for (int i=1; i<_array_size; i++)
         ASSERT_TRUE(a[i-1] <= a[i]) << a[i-1] << " is bigger than " << a[i];
 }
 
 //Tests CountingSort()
 TEST(CountingSort, NonUniqueRandom){
-    int a[array_size];
-    FillNonUniqueRandomNumbers(a, max);
-    CountingSort(a, array_size, max);
-    for (int i=1; i<array_size; i++)
+    int a[_array_size];
+    FillNonUniqueRandomNumbers(a, _max);
+    CountingSort(a, _array_size, _max);
+    for (int i=1; i<_array_size; i++)
         ASSERT_TRUE(a[i-1] <= a[i]) << a[i-1] << " is bigger than " << a[i];
 }
 
 //Tests RadixSort()
 TEST(RadixSort, NonUniqueRandom){
-    int a[array_size];
-    FillNonUniqueRandomNumbers(a, max);
-    RadixSort(a, array_size, max);
-    for (int i=1; i<array_size; i++)
+    int a[_array_size];
+    FillNonUniqueRandomNumbers(a, _max);
+    RadixSort(a, _array_size, _max);
+    for (int i=1; i<_array_size; i++)
         ASSERT_TRUE(a[i-1] <= a[i]) << a[i-1] << " is bigger than " << a[i];
 }
