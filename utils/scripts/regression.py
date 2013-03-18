593c42fd902a5789866ea9b002b2110ef98fb7f0
Denys S <denys.sobchyshak@gmail.com>
2013-03-18 13:38:12 +0100

    added regression line calculation signature

---
+++ utils/scripts/regression.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+    Copyright 2013 Denys Sobchyshak
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+__author__ = "Denys Sobchyshak"
+__email__ = "denys.sobchyshak@gmail.com"
+
+def main():
+    data = [(10,7),(4,6),(9,4)]
+
+def regressionLine(data):
+    """
+    Calculates regression line parameters using
+        m=(x'y'-(xy)')/((x')^2-(x^2)')
+        b=y'-m*x'
+    where x' is a mean of x values and so on. Thus a tuple is returned with (m,b) of the line y=mx+b.
+    """
+#    TODO: implement regression line calculator algorithm
+    return (0,0)
+
+if  __name__ == '__main__':main()
