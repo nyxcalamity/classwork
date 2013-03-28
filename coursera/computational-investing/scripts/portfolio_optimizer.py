623663c219902409bfbbc147b7107e3c8da71421
Denys S <denys.sobchyshak@gmail.com>
2013-03-28 17:59:34 +0100

    completed optimizer

--- coursera/computational-investing/scripts/portfolio_optimizer.py
+++ coursera/computational-investing/scripts/portfolio_optimizer.py
@@ -28,8 +28,30 @@ import numpy as np
 
 
 def main():
-#    testExamples()
-    optimizePortfolio()
+    quizQuestion1()
+    quizQuestion2()
+#    testExamples() #were checked to give correct results
+
+def quizQuestion1():
+    start_date = dt.datetime(2010,1,1)
+    end_date = dt.datetime(2010,12,31)
+    symbols = ['BRCM', 'TXN', 'IBM', 'HNZ']
+    optimal_portfolio = optimizePortfolio(start_date, end_date, symbols)
+
+    print '\nOptimal portfolio would be: '
+    displayResults(start_date,end_date,symbols,optimal_portfolio[0],optimal_portfolio[-1], optimal_portfolio[-2],\
+        optimal_portfolio[1], optimal_portfolio[2])
+
+def quizQuestion2():
+    start_date = dt.datetime(2011,1,1)
+    end_date = dt.datetime(2011,12,31)
+    symbols = ['BRCM', 'TXN', 'IBM', 'HNZ']
+    optimal_portfolio = optimizePortfolio(start_date, end_date, symbols)
+
+    print '\nOptimal portfolio would be: '
+    displayResults(start_date,end_date,symbols,optimal_portfolio[0],optimal_portfolio[-1], optimal_portfolio[-2],\
+        optimal_portfolio[1], optimal_portfolio[2])
+
 
 def testExamples():
     #First example
@@ -40,6 +62,16 @@ def testExamples():
     avg_daily_rets, cum_rets, volatility, sharpe = assesPerformance(start_date, end_date, symbols, allocations)
     displayResults(start_date,end_date,symbols,allocations,sharpe,volatility,avg_daily_rets,cum_rets)
 
+#    Expected results:
+#    Start Date: January 1, 2011
+#    End Date: December 31, 2011
+#    Symbols: ['AAPL', 'GLD', 'GOOG', 'XOM']
+#    Optimal Allocations: [0.4, 0.4, 0.0, 0.2]
+#    Sharpe Ratio: 1.02828403099
+#    Volatility (stdev of daily returns):  0.0101467067654
+#    Average Daily Return:  0.000657261102001
+#    Cumulative Return:  1.16487261965
+
     #Second example
     start_date = dt.datetime(2010,1,1)
     end_date = dt.datetime(2010,12,31)
@@ -47,6 +79,16 @@ def testExamples():
     allocations = [0.0, 0.0, 0.0, 1.0]
     avg_daily_rets, cum_rets, volatility, sharpe = assesPerformance(start_date, end_date, symbols, allocations)
     displayResults(start_date,end_date,symbols,allocations,sharpe,volatility,avg_daily_rets,cum_rets)
+
+#    Expected results:
+#    Start Date: January 1, 2010
+#    End Date: December 31, 2010
+#    Symbols: ['AXP', 'HPQ', 'IBM', 'HNZ']
+#    Optimal Allocations:  [0.0, 0.0, 0.0, 1.0]
+#    Sharpe Ratio: 1.29889334008
+#    Volatility (stdev of daily returns): 0.00924299255937
+#    Average Daily Return: 0.000756285585593
+#    Cumulative Return: 1.1960583568
 
 def displayResults(start_date,end_date,symbols,allocations,sharpe,volatility,avg_daily_rets,cum_rets):
     print 'Start Date: ' + start_date.strftime("%B %d, %Y")
@@ -59,25 +101,19 @@ def displayResults(start_date,end_date,symbols,allocations,sharpe,volatility,avg
     print 'Cumulative Return: ' + str(cum_rets)
     print '\n\n'
 
-def optimizePortfolio():
-    #We'll use data from first example
-    start_date = dt.datetime(2011,1,1)
-    end_date = dt.datetime(2011,12,31)
-    symbols = ['AAPL', 'GLD', 'GOOG', 'XOM']
-    allocations = [0.4, 0.4, 0.0, 0.2]
-    optimalPortfolio = (allocations,0,0,0,0) #tuple of max sharpe ratio and allocations
+def optimizePortfolio(start_date, end_date, symbols):
+    optimal_portfolio = ([],0,0,0,0) #tuple of (allocations, daily_rets_avg, cum_rets, volatility, sharpe_ratio)
     for i in range(11):
         print str(i)+'0% done'
         for j in range(11):
             for k in range(11):
                 for m in range(11):
-                    if i+j+k+m <= 10:
+                    if i+j+k+m == 10:
                         allocations=np.array([i,j,k,m])/10.0
-                        avg_daily_rets, cum_rets, volatility, sharpe = assesPerformance(start_date, end_date, symbols, allocations)
-                        if sharpe > optimalPortfolio[-1]:
-                            optimalPortfolio = (allocations, avg_daily_rets, cum_rets, volatility, sharpe)
-    print 'Optimal portfolio would be: '
-    displayResults(start_date,end_date,symbols,optimalPortfolio[0],optimalPortfolio[-1], optimalPortfolio[-2], optimalPortfolio[1], optimalPortfolio[2])
+                        daily_rets_avg, cum_rets, volatility, sharpe_ratio = assesPerformance(start_date, end_date, symbols, allocations)
+                        if sharpe_ratio > optimal_portfolio[-1]:
+                            optimal_portfolio = (allocations, daily_rets_avg, cum_rets, volatility, sharpe_ratio)
+    return optimal_portfolio
 
 
 def assesPerformance(start_date, end_date, symbols, allocations):
