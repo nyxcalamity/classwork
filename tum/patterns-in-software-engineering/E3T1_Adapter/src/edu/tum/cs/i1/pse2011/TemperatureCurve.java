70bc7b70133b50e79c0609103dc7a5e8bc1fbf32
Denys S <denys.sobchyshak@gmail.com>
2013-12-07 23:12:50 +0100

    completed adapter pattern task

--- tum/patterns-in-software-engineering/E3T1_Adapter/src/edu/tum/cs/i1/pse2011/TemperatureCurve.java
+++ tum/patterns-in-software-engineering/E3T1_Adapter/src/edu/tum/cs/i1/pse2011/TemperatureCurve.java
@@ -6,7 +6,7 @@ public class TemperatureCurve {
 
 	public static void main(String[] args) {
 		
-		ThermoInterface thermo = new CelsiusThermo();
+		ThermoInterface thermo = new ThermoFahrenheitAdapter();
 		DecimalFormat df = new DecimalFormat ("#.#");
 		
 		for (int i=0; i<5; i++) System.out.println(df.format(thermo.getTempC()));
