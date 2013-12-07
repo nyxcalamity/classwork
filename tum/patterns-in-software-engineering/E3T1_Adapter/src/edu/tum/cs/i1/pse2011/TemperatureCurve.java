3577fab1d22434056851fdb85b587f9efcd75c11
Denys S <denys.sobchyshak@gmail.com>
2013-12-07 22:45:02 +0100

    initial import of adapter task

---
+++ tum/patterns-in-software-engineering/E3T1_Adapter/src/edu/tum/cs/i1/pse2011/TemperatureCurve.java
@@ -0,0 +1,16 @@
+package edu.tum.cs.i1.pse2011;
+import java.text.*;
+
+
+public class TemperatureCurve {
+
+	public static void main(String[] args) {
+		
+		ThermoInterface thermo = new CelsiusThermo();
+		DecimalFormat df = new DecimalFormat ("#.#");
+		
+		for (int i=0; i<5; i++) System.out.println(df.format(thermo.getTempC()));
+
+	}
+
+}
---
+++ tum/patterns-in-software-engineering/E3T1_Adapter/src/edu/tum/cs/i1/pse2011/ThermoInterface.java
@@ -0,0 +1,7 @@
+package edu.tum.cs.i1.pse2011;
+
+public interface ThermoInterface {
+	
+	double getTempC (); // return Temp in Celsius
+	
+}
