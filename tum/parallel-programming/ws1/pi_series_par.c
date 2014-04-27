70f2a7b1de18e67c7d81315adec13bffeb3b4b4c
Denys S <denys.sobchyshak@gmail.com>
2014-04-27 12:45:10 +0200

    PP:fixed a bug with calculation

--- tum/parallel-programming/ws1/pi_series_par.c
+++ tum/parallel-programming/ws1/pi_series_par.c
@@ -5,14 +5,15 @@
 
 struct thread_data{
     int base,           //base term from which computation will start
-        num_elements;   //numer of terms to compute
+        num_terms;      //numer of terms to compute
     double result;
 };
 
 void *worker(void *arg){
     struct thread_data *data = arg;
     
-    for(int i=0;i<data->num_elements;i++){
+    data->result = 0.0;
+    for(int i=0;i<data->num_terms;i++){
         data->result += 1.0/((double) data->base*data->base);
         data->base++;
     }
@@ -32,30 +33,36 @@ double pi_series(long num_terms, long num_threads){
     
     pthread_t *thread;
     struct thread_data *data;
+    
+    //allocate space for threads and their data
     thread = malloc(num_threads*sizeof(*thread));
     data = malloc(num_threads*sizeof(*data));
     
-    
     for(unsigned long i=0;i<num_threads;i++){
-        data[i].result = 0.0;
         data[i].base = base;
-        data[i].num_elements = q;
-        (r > 0) ? data[i].num_elements++ : r--;
+        data[i].num_terms = q;
+        if(r > 0){
+            data[i].num_terms++; r--;
+        }
+
+        //create only as many threads as we need for current workload
+        if (data[i].num_terms > 0)
+            pthread_create(thread+i, NULL, worker, data+i);
         
-        pthread_create(thread+i, NULL, worker, data+i);
+        //should be thread safe, since it's a read operation and value shouldn't change
+        base += data[i].num_terms;
     }
     
     for(unsigned long i=0;i<num_threads;i++){
         //this subroutine blocks the calling thread until the specified thread terminates
+        //which means that main wont finish until all workers are done with their routines
         pthread_join(thread[i], NULL);
         sum += data[i].result;
     }        
 
     //uuu... there is no garbage collector :P
-    for (unsigned long i=0;i<num_threads;i++){
-        free(&thread[i]);
-        free(&data[i]);
-    }
+//    free(thread);
+//    free(data);
 
     return sqrt(6*sum);
 }
