eaca65917bd60326555d4b270492341087f3973e
Denys S <denys.sobchyshak@gmail.com>
2014-05-06 13:22:18 +0200

    PP:WS2:completed sequential algo

--- tum/parallel-programming/ws2/prime_count_seq.c
+++ tum/parallel-programming/ws2/prime_count_seq.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
 
-unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_threads, unsigned long chunk_size)
-{
-	return 1;
+
+unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_threads, 
+        unsigned long chunk_size){
+    int count=0;
+    for(int i=a; i<=b; i++){
+        if(i%2!=0){
+            int was_devisible_flag=0;
+            for(int j=3; j<=ceil(sqrt(i)); j+=2){
+                if(i%j==0 && !was_devisible_flag)
+                    was_devisible_flag=1;
+            }
+            
+            if(!was_devisible_flag)
+                count++;
+        }
+    }
+    return count;
 }
