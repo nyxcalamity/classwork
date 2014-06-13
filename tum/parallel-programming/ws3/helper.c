935201faec5072ad88e3e3a90420f1310dac16a5
Denys S <denys.sobchyshak@gmail.com>
2014-06-13 15:34:44 +0200

    pp:ws3:applied proper formatting to initial code

--- tum/parallel-programming/ws3/helper.c
+++ tum/parallel-programming/ws3/helper.c
@@ -3,36 +3,32 @@
 
 #define sign(x) ((x > 0) - (x < 0))
 
-int str_cmatch(const char* a, const char* b)
-{
-	int i = 0;
+int str_cmatch(const char* a, const char* b){
+    int i = 0;
 
-	while(a[i] != 0 && b[i] != 0 && a[i] == b[i])
-		i++;
+    while(a[i] != 0 && b[i] != 0 && a[i] == b[i])
+        i++;
 
-	return i;
+    return i;
 }
 
-struct timespec ts_diff(struct timespec a, struct timespec b)
-{
-	struct timespec t;
+struct timespec ts_diff(struct timespec a, struct timespec b){
+    struct timespec t;
 
-	t.tv_sec = a.tv_sec - b.tv_sec;
-	t.tv_nsec = a.tv_nsec - b.tv_nsec;
+    t.tv_sec = a.tv_sec - b.tv_sec;
+    t.tv_nsec = a.tv_nsec - b.tv_nsec;
 
-	a.tv_sec = abs(t.tv_sec) - 1 * ((sign(t.tv_sec) * sign(t.tv_nsec)) < 0);
-	a.tv_nsec = abs(1000000000 * ((sign(t.tv_sec) * sign(t.tv_nsec)) < 0) - abs(t.tv_nsec));
+    a.tv_sec = abs(t.tv_sec) - 1 * ((sign(t.tv_sec) * sign(t.tv_nsec)) < 0);
+    a.tv_nsec = abs(1000000000 * ((sign(t.tv_sec) * sign(t.tv_nsec)) < 0) - abs(t.tv_nsec));
 
-	return a;
+    return a;
 }
 
-double ts_to_double(struct timespec time)
-{
-	return time.tv_sec + time.tv_nsec / 1e9;
+double ts_to_double(struct timespec time){
+    return time.tv_sec + time.tv_nsec / 1e9;
 }
 
-int AlmostEqualRelative(double A, double B, double maxRelDiff)
-{
+int AlmostEqualRelative(double A, double B, double maxRelDiff){
     // Calculate the difference.
     double diff = fabs(A - B);
     A = fabs(A);
