17eeca1edcf40c9d45c9cbdf59a3d42d5907b81b
Denys S <denys.sobchyshak@gmail.com>
2015-07-11 17:06:04 +0200

    implemented a vk.com songs downloader

---
+++ utils/scripts/vk-downloader/README.md
@@ -0,0 +1,26 @@
+## Download your vk.com playlist
+---
+### Step-by-step guide
+These are exhaustive instrcutions that will convey a user from being forced to listen to music online to having all songs from his/her `vk.com` playlist downloaded onto a local hard drive. Let's get started.
+1. Get a [Firefox] browser
+2. Get [VK Music Playlist Download Add-on] from within your [Firefox] browser
+3. Install [python] version `3.4+`
+4. Open your playlist page in vk.com
+5. Scroll the page to the bottom (yes, this will be tedious) untill you will see the very first song you've ever added to `vk.com` - this will load the entire playlist on current page
+6. Use [VK Music Playlist Download Add-on] to download the playlist - it normally works by pressing the add-on button in the top right corner of your firefox
+7. Copy the downloaded playlist to the same directory where your `vk-downloader.py` is located
+8. Run the downloader in one of the following ways
+    - using default parameters - which means that your playlist is named `playlist.m3u` and your songs will be downloaded to `songs` which will be created under the directory of `vk-downloader.py`
+    - using customized parameters (lunch the script with `-h` to see more details) - you can specify where the playlist is and which folder to download the songs to as follows
+    ```
+    $ python3.4 ./vk-downloader.py -p ./playlist-den.m3u -t ../songs
+    ```
+9. Wait for the download to finish. This will take considerable time since the script is not running in parallel
+10. Enjoy your songs
+
+[Firefox]:https://www.mozilla.org/en-US/firefox/new/
+[VK Music Playlist Download Add-on]:https://addons.mozilla.org/en-US/firefox/addon/vk-music-playlist-download/
+[python]:https://www.python.org/downloads/
+
+#### Disclaimer
+Denys Sobchyshak DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL Denys Sobchyshak BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
---
+++ utils/scripts/vk-downloader/vk-downloader.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""
+    Copyright 2015 Denys Sobchyshak
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+__author__ = "Denys Sobchyshak"
+__email__ = "denys.sobchyshak@gmail.com"
+
+
+import logging
+import os.path
+import time
+import urllib.request as req
+import urllib.error
+from optparse import OptionParser
+
+
+def download(playlist_path, target_path):
+    logging.info('Parsing playlist')
+    with open(playlist_path) as f:
+        contents = f.read()
+        songs = []
+        urls = []
+        for l in contents.split('\n'):
+            if l.startswith('#EXTINF:'):
+                songs.append((l.split(',', maxsplit=1)[1]).replace('/', '').strip()+'.mp3')
+            if l.startswith('http'):
+                urls.append(l)
+    playlist = dict(zip(songs, urls))
+
+    logging.info('Downloading songs')
+    progress_counter = 0
+    song_counter = len(playlist)
+    progress(progress_counter/song_counter)
+    removed_songs = []
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+    for k, v in playlist.items():
+        try:
+            req.urlretrieve(v, os.path.join(target_path, k))
+        except ConnectionResetError as e:
+            # uuuuu look, they r smart....
+            time.sleep(2)
+            req.urlretrieve(v, os.path.join(target_path, k))
+        except urllib.error.HTTPError as e:
+            removed_songs.append(k)
+        progress_counter += 1
+        progress(progress_counter/song_counter)
+    print()
+    logging.info('Download complete')
+    logging.warning('Urls to next songs were removed: {}'.format(removed_songs))
+
+
+def progress(percentage):
+    print('\rProgress: {}%'.format(int(percentage*100)), end='')
+
+
+def initialize():
+    #set up logger
+    log_format = "%(asctime)s [%(levelname)s]:%(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=log_format)
+    #parse input args
+    parser = OptionParser()
+    parser.add_option('-p', '--playlist-file', dest='playlist_file', default='playlist.m3u',
+                      help='specifies a path to playlist', metavar='PLAYLIST_FILE')
+    parser.add_option('-t', '--target-dir', dest='target_dir', default='songs',
+                      help='specifies directory where playlist files are to be downloaded', metavar='PLAYLIST_FILE')
+    (options, args) = parser.parse_args()
+    #post process params
+    options.playlist_file = options.playlist_file.strip()
+    options.target_dir = options.target_dir.strip()
+    return options
+
+
+def terminate(msg):
+    logging.error(msg)
+    exit()
+
+
+if __name__ == '__main__':
+    opts = initialize()
+    download(opts.playlist_file, opts.target_dir)
